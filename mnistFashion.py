a280b4657ba37d695410ed0de308f4a63d4822dc
MarioMarkov <mario11septemvri@gmail.com>
2022-05-30 23:39:32 +0300

    find more ram for all 60,000 images

--- mnistFashion.py
+++ mnistFashion.py
@@ -4,11 +4,23 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense,Flatten
 
 
+#To get an image from training images -> data.load_data()[0][0][0][10] 
+data = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = data.load_data()
+
+#Normalizes the numbers from 0-255 to 0-1
+training_images = training_images/255.0
+test_images = test_images / 255.0
 
 model = Sequential([
     Flatten(input_shape=(28,28)),
-    Dense(128,activation=tf.nn.relu),
-    Dense(10,activation=tf.nn.softmax),
+    Dense(units=128,activation=tf.nn.relu),
+    Dense(units=10,activation=tf.nn.softmax),
 ])
 
-print(model)
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+
+model.fit(training_images,training_labels,epochs=5)
+
+model.evaluate(test_images,test_labels)
